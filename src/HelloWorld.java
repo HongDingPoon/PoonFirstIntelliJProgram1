05c8f188803d5a02551b002080b1438682de0b0c
1E10 Laptop <sample@bths.edu>
2022-09-21 12:14:57 -0400

    Added 2 print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] Args) {
+        System.out.print("Hello World!");
+        System.out.print("Hello World2!");
+        System.out.print("Hello World3!");
+    }
 }
